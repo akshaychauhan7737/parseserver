901396e3b63cf1752211696d0e817b88011eaa8c
Akshay <akshaychauhan7737@gmail.com>
2017-05-16 13:31:04 +0530

    message

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
 }
 
-var api = new ParseServer({mongodb:
+var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://admin:root@ds143141.mlab.com:43141/akshay',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'APPLICATION_ID',
